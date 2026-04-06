a9bca2ec9ae978254980b44f2e361836e218c49a
nazarenkovladik030507-art <nazarenko.vladik03.05.07@gmail.com>
2026-04-06 19:21:19 +0200

    custom-helpers.js

--- custom-helpers.js
+++ custom-helpers.js
@@ -1,18 +1,17 @@
 const axios = require('axios');
 
-module.exports = {
-  httpRequest: async function(options) {
+global.$helpers = {
+  httpRequest: async (options) => {
     try {
       const response = await axios({
         method: options.method || 'GET',
         url: options.url,
         data: options.body,
-        headers: options.headers || { 'Content-Type': 'application/json' },
-        timeout: options.timeout || 30000
+        headers: options.headers || { 'Content-Type': 'application/json' }
       });
       return response.data;
     } catch (error) {
-      throw new Error(`HTTP Request failed: ${error.message}`);
+      throw error;
     }
   }
 };
